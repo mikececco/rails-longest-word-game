947cf4bd880ec7a37b76cbea477558d16a249231
Mike Cecconello <mikececco2000@gmail.com>
2023-02-20 17:03:55 +0100

    Connected to API

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,15 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @letters = ('A'..'Z').to_a.sample(10)
   end
 
   def score
-    raise
+    word = params[:word]
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    user_serialized = URI.open(url).read
+    @result = JSON.parse(user_serialized)['found']
   end
 end
